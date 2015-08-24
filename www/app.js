26feaa84cf2954e8882b852d69fa14ec8b079e44
khhodges <ken@sipantic.net>
2015-08-24 16:26:12 -0400

    Remove Hybrid Index Page

--- www/app.js
+++ www/app.js
@@ -78,3 +78,4 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/browse');
 });
+alert('endappsjs');
